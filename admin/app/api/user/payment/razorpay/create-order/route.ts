0282d6a02aead46eedf0d4e3c6d75ff0fc330947
sandeep k. <buildbysandeep@gmail.com>
2026-08-11 09:02:06 +0530

    fix: comment out Razorpay client initialization in route.ts

--- admin/app/api/user/payment/razorpay/create-order/route.ts
+++ admin/app/api/user/payment/razorpay/create-order/route.ts
@@ -3,10 +3,12 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET!,
-});
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID!,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET!,
+// });
+
+const razorpay = {} as any;
 
 export async function POST(req: NextRequest) {
   try {
